89d6a35bb80179591f05c64427231afe6c755b5b
tuananh312 <nta0312@gmail.com>
2016-11-29 19:18:05 +0800

    add core functions and file input output for part 2

--- hmm.py
+++ hmm.py
@@ -3,24 +3,24 @@
 #emission param
 #storing position word position i with tag i
 
-words_x = []
-tags_y = []
 
-def populate_word_tag_lists(training_set):
-    training_set = readTraining(xyz)
+
+def populate_word_tag_lists(filename):
+    words_x = []
+    tags_y = []
+    training_set = readTraining(filename)
     for i in range(0,len(training_set)):
         for j in range(0,len(training_set[i])):
             words_x.add(training_set[i][j][0])
-            tags_y.add(training_set[i][j][1])        
-
-
+            tags_y.add(training_set[i][j][1])
+    return words_x, tags_y
 
 def count_emission_y_to_x(x,y):
-	count_yx = 0
-	for i in range (0, len(tags_y)):
-		if words_x[i] == y:
-			if tags_y[i] == x:
-				count += 1
+    count_yx = 0
+    for i in range (0, len(tags_y)):
+        if words_x[i] == y:
+            if tags_y[i] == x:
+                count_yx += 1
     return count_yx
 
 def count_emission_y(y):
@@ -31,8 +31,8 @@ def count_emission_y(y):
     return count_y
 
 
-def emission_param(x,y):
-    if x not in words_x:
+def emission_param(x,y,dev_in_word):
+    if x not in dev_in_word:
         #new word
         count_yx = 1
     else:
@@ -41,6 +41,11 @@ def emission_param(x,y):
     count_y = count_emission_y(y) + 1
     return count_emission_y_to_x / count_emission_y
 
+def create_emission_dict(words_x):
+    emission_dict = {}
+    for i in words_x:
+        emission_dict[i] = get_optimal_y(i)
+    return emission_dict
 
 def get_optimal_y(x):
     emission_param_set = {}
@@ -53,17 +58,48 @@ def get_optimal_y(x):
 #read training/testing data
 def readTraining(filename):
     training_set = []
-    file = open(filename, 'r').read()
-    for i in file.split("\n\n"):
+    fo = open(filename, 'r').read()
+    for i in fo.split("\n\n"):
         tweet = []
         for j in i.split("\n"):
             if len(j) == 0:
                 continue
             tweet.add(j.split("\t"))
         training_set.add(tweet)
+    fo.close()
     return training_set
 
+def readDevIn(filename):
+    dev_in_word = []
+    fo = open(filename, 'r')
+    while True:
+        line = fo.readline()
+        if line == '\n':
+            continue
+        elif line == ' ':
+            break
+        else:
+            dev_in_word.add(line)
+    fo.close()
+    return dev_in_word
 
+def make_prediction(training, dev_in):
+    prediction = {}
+    for i in dev_in:
+        prediction[i] = training[i]
+    return prediction
 
+def writePrediction(filename, result):
+    fo = open(filename, 'w')
+    for key in result:
+        fo.write(key)
+        fo.write(' ')
+        fo.write(result[key])
+        fo.write('\n')
+    fo.close()
 
-
+words_x, tags_y = populate_word_tag_lists('D:/Projects/ml-project-2016/EN/EN/train')
+dev_in_word = readDevIn('D:/Projects/ml-project-2016/EN/EN/dev.in')
+emission_dict = create_emission_dict(words_x)
+prediction_dict = make_prediction(emission_dict,dev_in_word)
+writePrediction('dev.p2.out',prediction_dict)
