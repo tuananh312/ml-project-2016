1613dc8e656731728042e220b1baa5af462c1f7a
Tuan Anh <nta0312@gmail.com>
2016-11-30 18:27:54 +0800

    fixed file reading, finished checking emission. checking result and writing file remaining

--- hmm.py
+++ hmm.py
@@ -9,48 +9,51 @@ def populate_word_tag_lists(filename):
     words_x = []
     tags_y = []
     training_set = readTraining(filename)
-    for i in range(0,len(training_set)):
-        for j in range(0,len(training_set[i])):
-            words_x.add(training_set[i][j][0])
-            tags_y.add(training_set[i][j][1])
+    for i in training_set:
+        if len(i) == 2:
+            words_x.append(i[0])
+            tags_y.append(i[1])
     return words_x, tags_y
 
 def count_emission_y_to_x(x,y):
     count_yx = 0
-    for i in range (0, len(tags_y)):
-        if words_x[i] == y:
-            if tags_y[i] == x:
+    for i in range(0, len(tags_y)):
+        if words_x[i] == x:
+            if tags_y[i] == y:
                 count_yx += 1
     return count_yx
 
 def count_emission_y(y):
     count_y = 0
-    for i in range (0, len(tags_y)):
-        if tags_y == y:
+    for i in tags_y:
+        if i == y:
             count_y += 1
     return count_y
 
 
-def emission_param(x,y,dev_in_word):
-    if x not in dev_in_word:
-        #new word
+def emission_param(x,y):
+    count_yx = count_emission_y_to_x(x,y)
+    count_y = count_emission_y(y)
+    return count_yx / count_y
+
+def emission_param_fixed(x,y):
+    if x not in words_x:
         count_yx = 1
     else:
-        #not new
         count_yx = count_emission_y_to_x(x,y)
     count_y = count_emission_y(y) + 1
-    return count_emission_y_to_x / count_emission_y
+    return count_yx / count_y
 
-def create_emission_dict(words_x):
+def create_emission_dict(dev_in_word):
     emission_dict = {}
-    for i in words_x:
+    for i in dev_in_word:
         emission_dict[i] = get_optimal_y(i)
     return emission_dict
 
 def get_optimal_y(x):
     emission_param_set = {}
     for y in tags_y:
-        emission_param_set[y] = emission_param(x,y)
+        emission_param_set[y] = emission_param_fixed(x,y)
     optimal_y = max(emission_param_set, key=emission_param_set.get)
     return optimal_y
 
@@ -58,14 +61,15 @@ def get_optimal_y(x):
 #read training/testing data
 def readTraining(filename):
     training_set = []
-    fo = open(filename, 'r').read()
-    for i in fo.split("\n\n"):
-        tweet = []
-        for j in i.split("\n"):
-            if len(j) == 0:
-                continue
-            tweet.add(j.split("\t"))
-        training_set.add(tweet)
+    fo = open(filename, 'r')
+    while True:
+        line = fo.readline()
+        if line == '\n':
+            continue
+        elif line == '':
+            break
+        else:
+            training_set.append(line.replace('\r\n','').split(' '))
     fo.close()
     return training_set
 
@@ -76,18 +80,18 @@ def readDevIn(filename):
         line = fo.readline()
         if line == '\n':
             continue
-        elif line == ' ':
+        elif line == '':
             break
         else:
-            dev_in_word.add(line)
+            dev_in_word.append(line.replace('\r\n',''))
     fo.close()
     return dev_in_word
 
-def make_prediction(training, dev_in):
-    prediction = {}
-    for i in dev_in:
-        prediction[i] = training[i]
-    return prediction
+# def make_prediction(training, dev_in):
+#     prediction = {}
+#     for i in dev_in:
+#         prediction[i] = training[i]
+#     return prediction
 
 def writePrediction(filename, result):
     fo = open(filename, 'w')
@@ -98,8 +102,10 @@ def writePrediction(filename, result):
         fo.write('\n')
     fo.close()
 
-words_x, tags_y = populate_word_tag_lists('D:/Projects/ml-project-2016/EN/EN/train')
-dev_in_word = readDevIn('D:/Projects/ml-project-2016/EN/EN/dev.in')
-emission_dict = create_emission_dict(words_x)
-prediction_dict = make_prediction(emission_dict,dev_in_word)
-writePrediction('dev.p2.out',prediction_dict)
+words_x, tags_y = populate_word_tag_lists('/home/tuananh/Documents/ML/Project/ml-project-2016/EN/EN/train')
+dev_in_word = readDevIn('/home/tuananh/Documents/ML/Project/ml-project-2016/EN/EN/dev.in')
+emission_dict = create_emission_dict(dev_in_word)
+# prediction_dict = make_prediction(emission_dict,dev_in_word)
+# writePrediction('dev.p2.out',prediction_dict)
+print len(words_x), len(tags_y)
+print emission_dict
